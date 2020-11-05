ac5bfe0f6aeefb8c3800c3a8e4fe96285f213ba5
Jose Carvajal <jose.carvajal@MacBook-Pro-de-Jose.local>
2020-11-05 12:33:28 -0500

    implementing repository structre and first approach to the test

---
+++ test/features/number_trivia/domain/usescases/get_concrete_number_trivia.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/mockito.dart';
+import 'package:trivia_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+class MockNumberTriviaRepository extends Mock implements NumberTriviaRepository{
+  
+}
